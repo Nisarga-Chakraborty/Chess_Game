fc0e0d56c10830fab63d466703f354567ecfc82b
Nisarga Chakraborty <nisargac@op.iitg.ac.in>
2025-11-02 04:34:23 +0530

    Add Move class to represent a move in a grid

---
+++ Move.java
@@ -0,0 +1,10 @@
+public class Move {
+    public int srcRow, srcCol, destRow, destCol;
+
+    public Move(int srcRow, int srcCol, int destRow, int destCol) {
+        this.srcRow = srcRow;
+        this.srcCol = srcCol;
+        this.destRow = destRow;
+        this.destCol = destCol;
+    }
+}
